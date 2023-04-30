f12a394ab332a5f7987b7454163ab9e80ce8a65d
shashankvivek <shashankvivek.7@gmail.com>
2023-04-30 21:46:14 +0200

    first migration

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+
+
+namespace dotnet_rpg.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Character> Characters => Set<Character>();
+    }
+}
